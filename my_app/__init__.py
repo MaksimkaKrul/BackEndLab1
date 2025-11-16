325fbce6d670696b90aaf46e48efce7ca8465af2
Makc Krulevskiy <krulevckiym@gmail.com>
2025-11-16 23:58:11 +0200

    refactor: Restructure project into app factory pattern

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,5 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_smorest import Api
+from config import Config
 
-app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate()
 
-from my_app import views
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    api = Api(app)
+
+    from my_app.resources.user import blp as UserBlueprint
+    from my_app.resources.category import blp as CategoryBlueprint
+    from my_app.resources.record import blp as RecordBlueprint
+    from my_app.resources.currency import blp as CurrencyBlueprint
+
+    api.register_blueprint(UserBlueprint)
+    api.register_blueprint(CategoryBlueprint)
+    api.register_blueprint(RecordBlueprint)
+    api.register_blueprint(CurrencyBlueprint)
+    
+    return app
