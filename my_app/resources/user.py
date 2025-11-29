61aca87359611e218220c99a3a80623d1fa50f39
Makc Krulevskiy <krulevckiym@gmail.com>
2025-11-29 04:51:02 +0200

    implement registration and login endpoints with password hashing

--- my_app/resources/user.py
+++ my_app/resources/user.py
@@ -1,58 +1,25 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 from my_app import db
 from my_app.models import UserModel
-from my_app.schemas import PlainUserSchema, UserSchema
+from my_app.schemas import PlainUserSchema, UserSchema, UserLoginSchema
 
 blp = Blueprint("Users", "users", description="Operations on users")
 
-
-@blp.route("/user/<int:user_id>")
-class User(MethodView):
-    @blp.response(200, UserSchema)
-    def get(self, user_id):
-        user = UserModel.query.get_or_404(user_id)
-        return user
-
-    def delete(self, user_id):
-        user = UserModel.query.get_or_404(user_id)
-        db.session.delete(user)
-        db.session.commit()
-        return {"message": "User deleted."}
-
-    @blp.arguments(PlainUserSchema)
-    @blp.response(200, UserSchema)
-    def put(self, user_data, user_id):
-        user = UserModel.query.get_or_404(user_id)
-
-        if "name" in user_data:
-            user.name = user_data["name"]
-        if "default_currency_id" in user_data:
-            user.default_currency_id = user_data["default_currency_id"]
-
-        try:
-            db.session.add(user)
-            db.session.commit()
-        except IntegrityError:
-            abort(400, message="A user with that name already exists.")
-        except SQLAlchemyError:
-            abort(500, message="An error occurred while updating the user.")
-            
-        return user
-
-
-@blp.route("/user")
-class UserList(MethodView):
-    @blp.response(200, UserSchema(many=True))
-    def get(self):
-        return UserModel.query.all()
-    
+@blp.route("/register")
+class UserRegister(MethodView):
     @blp.arguments(PlainUserSchema)
     @blp.response(201, UserSchema)
     def post(self, user_data):
-        user = UserModel(**user_data)
+        user = UserModel(
+            name=user_data["name"],
+            password=pbkdf2_sha256.hash(user_data["password"]),
+            default_currency_id=user_data.get("default_currency_id")
+        )
         try:
             db.session.add(user)
             db.session.commit()
@@ -61,3 +28,37 @@ class UserList(MethodView):
         except SQLAlchemyError:
             abort(500, message="An error occurred while inserting the user.")
         return user
+
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserLoginSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(UserModel.name == user_data["name"]).first()
+
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            access_token = create_access_token(identity=str(user.id))
+            return {"access_token": access_token}
+
+        abort(401, message="Invalid credentials.")
+
+@blp.route("/user/<int:user_id>")
+class User(MethodView):
+    @blp.response(200, UserSchema)
+    @jwt_required()
+    def get(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        return user
+
+    @jwt_required()
+    def delete(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+        return {"message": "User deleted."}
+
+@blp.route("/user")
+class UserList(MethodView):
+    @blp.response(200, UserSchema(many=True))
+    @jwt_required()
+    def get(self):
+        return UserModel.query.all()
